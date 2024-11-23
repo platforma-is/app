3c7839fff01c56dcc8c4adc6a36dc4a92b92af31
StAl15 <stihinaleksander@yandex.ru>
2024-11-23 12:43:22 +0500

    исправил юай таблицы

--- components/form/responses/Responses.tsx
+++ components/form/responses/Responses.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import { Paper, Table, Text } from "@mantine/core";
 import { ResponseItem } from "@/components/form/responses/ResponseItem/ResponseItem";
 import { Response } from "@/shared/api/model";
+import { useWindowSize } from "@/shared/hooks/useWindowSize";
 
 type ResponsesProps = {
   keys: string[];
@@ -9,38 +10,45 @@ type ResponsesProps = {
 };
 
 export const Responses: FC<ResponsesProps> = ({ responses, keys }) => {
+  const { width, height } = useWindowSize();
   return responses.length > 0 ? (
     <Paper radius={"md"} mt={"1.5rem"}>
-      <Table
-        highlightOnHover
-        bgcolor={"white"}
-        stickyHeader
-        stickyHeaderOffset={60}
-        verticalSpacing={"0.75rem"}
-        horizontalSpacing={"0.75rem"}
+      <Table.ScrollContainer
+        mah={height - 300}
+        minWidth={width - 300}
+        type="native"
       >
-        <Table.Thead>
-          <Table.Tr>
-            {keys.map((key) => (
-              <Table.Th key={key}>
+        <Table
+          highlightOnHover
+          bgcolor={"white"}
+          stickyHeader
+          stickyHeaderOffset={0}
+          verticalSpacing={"0.75rem"}
+          horizontalSpacing={"0.75rem"}
+        >
+          <Table.Thead>
+            <Table.Tr>
+              {keys.map((key) => (
+                <Table.Th key={key}>
+                  <Text color={"#A6A7AB"} size={"0.75rem"}>
+                    {key}
+                  </Text>
+                </Table.Th>
+              ))}
+              <Table.Th w={"2rem"} key={"createdAt"} pl={"1rem"}>
                 <Text color={"#A6A7AB"} size={"0.75rem"}>
-                  {key}
+                  Дата и время
                 </Text>
               </Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {responses?.map((response) => (
+              <ResponseItem keys={keys} key={response.id} response={response} />
             ))}
-            <Table.Th key={"createdAt"} pl={"1rem"}>
-              <Text color={"#A6A7AB"} size={"0.75rem"}>
-                Дата и время
-              </Text>
-            </Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {responses?.map((response) => (
-            <ResponseItem keys={keys} key={response.id} response={response} />
-          ))}
-        </Table.Tbody>
-      </Table>
+          </Table.Tbody>
+        </Table>
+      </Table.ScrollContainer>
     </Paper>
   ) : (
     <Text
