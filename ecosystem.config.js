ad71d9bd0bc52d9aae4b0c1a0994d67828f3ef43
Filat Astakhov <ph1l74rus@gmail.com>
2024-11-29 16:10:32 +0300

    Deplot fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       "name": "Platforma Frontend",
       "script": "./node_modules/next/dist/bin/next",
-      "env": {
+      "env_prod": {
         "SECRET": "l__adsfjlkajsd!fh1",
         "AUTH_SUCCESS_CALLBACK_URL": "https://platforma.is/",
         "BACKEND_HOSTNAME": "https://platforma.is/backend-api",
@@ -17,7 +17,23 @@ module.exports = {
         "YANDEX_CLIENT_SECRET": "f8969910ea04425980c01c2b84bb7b9e",
         "DATABASE_URL": "postgres://postgres:pgpwd@platforma.is:5438/postgres?connect_timeout=300",
         "NEXT_PUBLIC_API": "https://platforma.is/backend-api",
-        "NEXTAUTH_URL": "https://platforma.is"
+        "NEXTAUTH_URL": "https://platforma.is",
+      },
+      "env_test": {
+        "SECRET": "l__adsfjlkajsd!fh1",
+        "AUTH_SUCCESS_CALLBACK_URL": "https://test.platforma.is/",
+        "BACKEND_HOSTNAME": "https://test.platforma.is/backend-api",
+        "BACKEND_PORT": 5002,
+        "GITHUB_ID": "7cd2b1344c18a034bfc3",
+        "GITHUB_SECRET": "6066f2a8a25fce03d5d12690a1cb8a62d6444de3",
+        "VK_ID": 51882834,
+        "VK_CLIENT_ID": "QkyiNBpoXxn1OEk4uF6G",
+        "VK_CLIENT_SECRET": "64c76a1064c76a1064c76a100367d0c142664c764c76a10013d8b31cf610c7fc1a0d6d1",
+        "YANDEX_CLIENT_ID": "1433b1a875cc47c2b9d78e93cb55791e",
+        "YANDEX_CLIENT_SECRET": "f8969910ea04425980c01c2b84bb7b9e",
+        "DATABASE_URL": "postgres://postgres:pgpwd@platforma.is:5438/postgres?connect_timeout=300",
+        "NEXT_PUBLIC_API": "https://test.platforma.is/backend-api",
+        "NEXTAUTH_URL": "https://test.platforma.is",
       },
       "args": "-p 3000",
       "exec_mode": "cluster",
---
+++ prisma/migrations/20241123084711_2024_11_23_dev_01/migration.sql
@@ -0,0 +1,117 @@
+-- CreateEnum
+CREATE TYPE "SucessScreen" AS ENUM ('STANDART', 'CUSTOM');
+
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    "oauth_token_secret" TEXT,
+    "oauth_token" TEXT,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "session_token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "email_verified" TIMESTAMP(3),
+    "image" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "phone" TEXT,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "verificationtokens" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "forms" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "keys" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "folder" TEXT,
+    "spamProtected" BOOLEAN NOT NULL DEFAULT false,
+    "emailNotification" BOOLEAN NOT NULL DEFAULT false,
+    "emailNotificationMails" TEXT[],
+    "successScreen" "SucessScreen" DEFAULT 'STANDART',
+    "customUrlSuccess" TEXT,
+    "customUrlError" TEXT,
+
+    CONSTRAINT "forms_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "responses" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "formId" TEXT NOT NULL,
+    "data" JSONB NOT NULL,
+
+    CONSTRAINT "responses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_phone_key" ON "users"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verificationtokens_token_key" ON "verificationtokens"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verificationtokens_identifier_token_key" ON "verificationtokens"("identifier", "token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "forms_id_key" ON "forms"("id");
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "forms" ADD CONSTRAINT "forms_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "responses" ADD CONSTRAINT "responses_formId_fkey" FOREIGN KEY ("formId") REFERENCES "forms"("id") ON DELETE CASCADE ON UPDATE CASCADE;
